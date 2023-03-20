8e28072dc88a35a6bb3676639fee9f0ff6e0ab96
Yash Anand <yashanand1910@gmail.com>
2023-03-20 17:57:23 -0400

    Update config on Mon Mar 20 17:57:23 EDT 2023

--- config.lua
+++ config.lua
@@ -397,6 +397,7 @@ lvim.plugins = {
     lazy = false,
 		config = function()
 			vim.g["tex_flavor"] = "latex"
+      vim.g["vimtex_compiler_method"] = "latexmk"
 			vim.g["vimtex_view_method"] = "skim"
 			vim.g["vimtex_view_skim_sync"] = 1
 			vim.g["vimtex_view_skim_activate"] = 1
