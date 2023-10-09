998e6ce59f0f0d087c57d441f5901804fa6d5ecd
Yash Anand <yashanand1910@gmail.com>
2023-10-09 12:27:45 -0400

    Update config on Mon Oct  9 12:27:45 EDT 2023

--- nvim/lua/lsp/formatter.lua
+++ nvim/lua/lsp/formatter.lua
@@ -5,17 +5,18 @@
 local formatter = require("conform")
 
 formatter.setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    json = { "prettier" },
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
-    python = {
-      formatters = { "isort", "black" },
-      run_all_formatters = true,
-    },
-    c = { "clang_format" },
-    cpp = { "clang_format" }
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		json = { "prettier" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		typescriptreact = { "prettier" },
+		python = {
+			formatters = { "isort", "black" },
+			run_all_formatters = true,
+		},
+		c = { "clang_format" },
+		cpp = { "clang_format" },
+		tex = { "latexindent" },
+	},
 })
