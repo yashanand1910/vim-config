0aceda5ef11c694d0feec0b4a3fdf121c99a38ba
Yash Anand <yashanand1910@gmail.com>
2024-07-15 16:55:16 -0400

    Update config on Mon Jul 15 04:55:16 PM EDT 2024

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -22,11 +22,6 @@ endtry
 
 local colors = require("tokyonight.colors")
 if vim.g.colors_name == "tokyonight" then
-	-- Set bright colors for line numbers
-	vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFFFFF" })
-	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.default.dark5 })
-	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.default.dark5 })
-
 	-- Set bright colors for Comments
 	vim.api.nvim_set_hl(0, "Comment", { fg = colors.default.dark5, italic = true })
 end
